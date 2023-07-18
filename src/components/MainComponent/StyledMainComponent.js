5c679555ca4f0d6a5dea4ad56a20a3cc6170c1d2
Szymon Bacanski <s.bacanski@gmail.com>
2023-07-18 20:57:48 +0200

    new conditions and detail refactor
    Two new conditions in 'reducerMainComponent' and detail refactor in 'generatePassword' code (parsing a number of loop repetition to INT and smaller operator)

--- src/components/MainComponent/StyledMainComponent.js
+++ src/components/MainComponent/StyledMainComponent.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const StyledMainComponent = styled.div`
-  border: 1px solid black;
+  /* border: 1px solid black; */
   box-shadow: 0px 0px 21px 1px rgba(0, 0, 0, 0.25);
 
   display: flex;
